e2145e4d04d86a2346b463592f76b048ce8bf41b
asakasa <asakasa@me.com>
2014-06-17 12:37:30 +0900

    各ボタン処理，ギャラリー選択処理の追加
    loadImageButton: ギャラリーから画像を選択するインテント処理を追加
    setServiceButton: WallPaperServiceサービスの起動
    unsetServiceButton: WallPaperServiceサービスの停止

--- BBLink/src/jp/ac/kansai_u/kutc/BBLink/MainActivity.java
+++ BBLink/src/jp/ac/kansai_u/kutc/BBLink/MainActivity.java
@@ -1,9 +1,18 @@
 package jp.ac.kansai_u.kutc.BBLink;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity{
+/**
+ * アプリケーションのスタートとなるアクティビティ
+ * @author akasaka
+ */
+public class MainActivity extends Activity implements View.OnClickListener{
+    final int REQUEST_GALALLY_IMAGE = 0x12FCEA7;  // ギャラリーインテント時の返却値（任意の数値）
+    Intent wallPaperService = null;  // WallPaperServiceクラス起動用のインテント
     /**
      * Called when the activity is first created.
      */
@@ -11,6 +20,16 @@ public class MainActivity extends Activity{
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button loadImageButton = (Button)findViewById(R.id.loadImageButton);
+        Button setServiceButton = (Button)findViewById(R.id.setServiceButton);
+        Button unsetServiceButton = (Button)findViewById(R.id.unsetServiceButton);
+        loadImageButton.setOnClickListener(this);
+        setServiceButton.setOnClickListener(this);
+        unsetServiceButton.setOnClickListener(this);
+
+        // サービス起動用のインテント
+        wallPaperService = new Intent(this, WallPaperService.class);
     }
 
     @Override
@@ -41,5 +60,33 @@ public class MainActivity extends Activity{
     @Override
     protected void onDestroy(){
         super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v.getId() == R.id.loadImageButton){
+            // ギャラリーから画像を取得する
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");  // 画像タイプに限定
+            startActivityForResult(intent, REQUEST_GALALLY_IMAGE);
+        }else if(v.getId() == R.id.setServiceButton){
+            startService(wallPaperService);
+        }else if(v.getId() == R.id.unsetServiceButton){
+            stopService(wallPaperService);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode){
+            case REQUEST_GALALLY_IMAGE:
+                if(resultCode != RESULT_OK) return;
+                // ここから下，取得したデータ処理ダバー
+                break;
+            default:
+                break;
+        }
     }
 }
