b80212d6a3f6696b8a4a956fcff749fbcae663b9
asakasa <asakasa@me.com>
2014-06-19 00:18:04 +0900

    メニュー選択後のポップウィンドウに関する処理の骨組みを追加
    背景画像及びレイアウトは変更の可能性大
    setTextで表示する文章は，ファイルから読み込めるようにする予定

--- BBLink/src/jp/ac/kansai_u/kutc/BBLink/MainActivity.java
+++ BBLink/src/jp/ac/kansai_u/kutc/BBLink/MainActivity.java
@@ -1,12 +1,17 @@
 package jp.ac.kansai_u.kutc.BBLink;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.View;
+import android.view.*;
 import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 /**
  * アプリケーションのスタートとなるアクティビティ
@@ -97,5 +102,54 @@ public class MainActivity extends Activity implements View.OnClickListener{
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.action_bar_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        PopupWindow popupWindow = createPopupWindow();
+        TextView popupTitle = (TextView)popupWindow.getContentView().findViewById(R.id.popup_title);
+        TextView popupBody = (TextView)popupWindow.getContentView().findViewById(R.id.popup_body);
+        switch(item.getItemId()){
+            case R.id.menu_info:
+                // Whant's app
+                popupTitle.setText("このアプリについて");
+                popupBody.setText("What's app");
+                popupWindow.showAtLocation(this.findViewById(R.id.menu_info), Gravity.CENTER, 0, 0);
+                break;
+            case R.id.menu_help:
+                // Help
+                popupTitle.setText("ヘルプ");
+                popupBody.setText("HELP");
+                popupWindow.showAtLocation(this.findViewById(R.id.menu_info), Gravity.CENTER, 0, 0);
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * ポップウィンドウを作成する
+     * @return 作成したポップウィンドウ
+     */
+    private PopupWindow createPopupWindow(){
+        LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        // /res/layout/popup.xmlからレイアウト情報を引っ張ってくる
+        View pv = inflater.inflate(R.layout.popup, null);
+        pv.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        PopupWindow pw = new PopupWindow();
+        // 背景の設定，これを設定しないと外側タッチで非表示という処理が正常に動作しなくなる
+        pw.setBackgroundDrawable(getResources().getDrawable(R.drawable.popup_bg));
+        pw.setContentView(pv);
+        // ポップウィンドウの外側をタッチすると非表示にする
+        pw.setOutsideTouchable(true);
+        // ポップウィンドウ表示後フォーカスを強制するか否か（他のコンポーネントをタッチできるか否か）
+//        pw.setFocusable(true);
+        pw.setWidth(WindowManager.LayoutParams.WRAP_CONTENT);
+        pw.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
+        return pw;
     }
 }
