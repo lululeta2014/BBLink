533f4b4a434f787dcc9d3fdc8ae5b16b4e4f8201
asakasa <asakasa@me.com>
2014-07-13 16:52:13 +0900

    ヘルプ画面とクレジット画面の追加

---
+++ BBLink/src/jp/ac/kansai_u/kutc/BBLink/WhatsApp/CreditActivity.java
@@ -0,0 +1,15 @@
+package jp.ac.kansai_u.kutc.BBLink.WhatsApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * クレジット画面
+ * @author akasaka
+ */
+public class CreditActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ BBLink/src/jp/ac/kansai_u/kutc/BBLink/WhatsApp/HelpActivity.java
@@ -0,0 +1,15 @@
+package jp.ac.kansai_u.kutc.BBLink.WhatsApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * ヘルプ画面
+ * @author akasaka
+ */
+public class HelpActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
